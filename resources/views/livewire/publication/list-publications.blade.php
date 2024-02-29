9cfbb409912b1f33a254b5dc1c8a21868b42810e
pmrober <pmrober@gmail.com>
2024-02-29 10:48:37 +0100

    290224_1

--- resources/views/livewire/publication/list-publications.blade.php
+++ resources/views/livewire/publication/list-publications.blade.php
@@ -48,8 +48,13 @@
                         </button>
                         
                         <div>
-                            <livewire:like-publication :publication="$publication" />
                             
+                            <div>
+                                <button wire:click="like({{$publication}})" class="btn btn-icon-lik-header btn-color-active-gray-500 {{ $isLiked ? 'btn-color-danger' : 'btn-color-gray-500'}} w-40px h-40px w-md-35px h-md-35px">
+                                    <i class="bi bi-hand-thumbs-up fs-1"></i>
+                                </button>
+                            </div>
+                              
                         </div>
                         
                         
